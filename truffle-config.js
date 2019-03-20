0c0418d072b6a5136068c67c0e1260fff97eb86b
Christoph <ignatz@gmail.com>
2019-03-20 19:21:49 -0400

    fixed memory leak

--- truffle-config.js
+++ truffle-config.js
@@ -1,31 +1,41 @@
 require('dotenv').config();
 require('babel-register');
 require('babel-polyfill');
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
-// var HDWalletProvider = require("truffle-hdwallet-provider");
-
-const path = require("path");
+// const path = require("path");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "./client/src/contracts"),
-  compilers: {
-    solc: {
-      version: "0.5.2"
-    }
-  },
-  mocha: {
-    useColors: true
-  },
+  // contracts_build_directory: path.join(__dirname, "./client/src/contracts"),
+  // compilers: {
+  //   solc: {
+  //     version: "0.5.2"
+  //   }
+  // },
+  // mocha: {
+  //   useColors: true
+  // },
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
-    test: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+    ropsten:  {
+      network_id: 3,
+      host: "localhost",
+      port:  8545,
+      gas:   4600000
+    },
+    infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/2b0cafabdc904bada4af38e6ab9ef990");
+      },
+      network_id: 3
     }
+  },
+  rpc: {
+    host: 'localhost',
+    post:8080
   }
 };
